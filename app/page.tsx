4fb76462d37d763e3c82057b11be66faac1796df
Martin Lowe <martinlowe673@gmail.com>
2024-03-04 19:50:07 +0000

    chat ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
+"use client"
+
+import {useChat} from "ai/react"
 
 export default function Home() {
+  const {messages, input, handleInputChnage, handleSubmit} = useChat(
+    {
+      api:"/api",
+    }
+  );
   return (
-   <h1>Hello from the indec</h1>
+    <div className="min-h-screen bg-neutral-800">
+        {messages.length !== 0 ?(
+          <div>
+
+          </div>
+        ):(
+          <div className="w-full flex justify-center pt-32">
+
+          </div>
+        )}
+    </div>
   );
 }
