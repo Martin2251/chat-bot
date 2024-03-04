7becdb1a1e8615896199b147ba0412a74dcc0a51
Martin Lowe <martinlowe673@gmail.com>
2024-03-04 19:38:59 +0000

    layout and route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,4 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
 import { Configuration, OpenAIApi } from "openai-edge";
 
 const config = new Configuration({
@@ -11,5 +12,13 @@ export const runtime = "edge"
 export async function POST(req:Request){
     const {message} = await req.json()
 
-    const response = 
+    const response = await openai.createChatCompletion({
+        model:"gpt-3.5-turbo",
+        stream:true,
+        messages:message
+    })
+
+    const stream = OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
 }
